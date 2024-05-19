bb7342ff8901105ddd2da83965c8728f0f8eea3e
Quillraven <quillraven@gmail.com>
2024-05-20 00:10:59 +0200

    tiled editor initial files

---
+++ assets/maps/objects.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="objects" tilewidth="16" tileheight="16" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="MapObject">
+  <properties>
+   <property name="atlas" propertytype="AtlasAsset" value="CHARACTERS"/>
+   <property name="atlasRegionKey" value="hero/idle_down"/>
+   <property name="objType" propertytype="MapObjectType" value="HERO"/>
+  </properties>
+  <image width="16" height="16" source="objects/hero.png"/>
+ </tile>
+</tileset>
