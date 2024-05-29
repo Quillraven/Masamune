b60a07d40343903a4882aeaffa2a50189d8c5c1c
Quillraven <quillraven@gmail.com>
2024-05-29 21:43:33 +0200

    add CameraSystem test

---
+++ core/src/test/kotlin/io/github/masamune/CameraTest.kt
@@ -0,0 +1,116 @@
+package io.github.masamune
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.assets.loaders.resolvers.ClasspathFileHandleResolver
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.maps.tiled.TiledMap
+import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.utils.viewport.ExtendViewport
+import com.badlogic.gdx.utils.viewport.Viewport
+import com.github.quillraven.fleks.World
+import com.github.quillraven.fleks.configureWorld
+import io.github.masamune.asset.AssetService
+import io.github.masamune.asset.AtlasAsset
+import io.github.masamune.component.Tag
+import io.github.masamune.component.Transform
+import io.github.masamune.event.EventService
+import io.github.masamune.system.AnimationSystem
+import io.github.masamune.system.CameraSystem
+import io.github.masamune.system.DebugPhysicRenderSystem
+import io.github.masamune.system.RenderSystem
+import io.github.masamune.tiledmap.TiledService
+import ktx.app.KtxApplicationAdapter
+import ktx.app.KtxInputAdapter
+import ktx.app.clearScreen
+import ktx.assets.disposeSafely
+import ktx.box2d.createWorld
+import ktx.math.vec2
+
+fun main() {
+    Lwjgl3Application(CameraTest(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("Camera Test (click to move; mousewheel to zoom)")
+        setWindowedMode(1280, 960)
+    })
+}
+
+private class CameraController(
+    private val gameViewport: Viewport,
+    private val world: World
+) : KtxInputAdapter {
+    private val camera = gameViewport.camera as OrthographicCamera
+    private val camEntity = world.family { all(Tag.CAMERA_FOCUS) }.first()
+
+    override fun scrolled(amountX: Float, amountY: Float): Boolean {
+        camera.zoom += amountY
+        return true
+    }
+
+    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        with(world) {
+            val (position, size) = camEntity[Transform]
+            val worldCoords = gameViewport.unproject(vec2(screenX.toFloat(), screenY.toFloat()))
+            position.set(worldCoords.sub(size.cpy().scl(0.5f)), 0f)
+        }
+        return true
+    }
+}
+
+class CameraTest : KtxApplicationAdapter {
+    private val batch: Batch by lazy { SpriteBatch() }
+    private val gameViewport: Viewport = ExtendViewport(8f, 4.5f)
+    private val world by lazy { gameWorld() }
+    private val assetService by lazy { AssetService(ClasspathFileHandleResolver()) }
+    private val eventService by lazy { EventService() }
+    private val tiledService by lazy { TiledService(assetService, eventService) }
+    private lateinit var tiledMap: TiledMap
+
+    private fun gameWorld() = configureWorld {
+        injectables {
+            add(batch)
+            add(gameViewport)
+            add(createWorld(gravity = Vector2.Zero))
+        }
+
+        systems {
+            add(AnimationSystem())
+            add(CameraSystem())
+            add(RenderSystem())
+            add(DebugPhysicRenderSystem())
+        }
+    }
+
+    override fun create() {
+        eventService += world
+
+        assetService.load(AtlasAsset.CHARS_AND_PROPS)
+        assetService.finishLoading()
+        tiledMap = TmxMapLoader(ClasspathFileHandleResolver()).load("maps/test.tmx")
+        tiledService.setMap(tiledMap, world)
+
+        Gdx.input.inputProcessor = CameraController(gameViewport, world)
+    }
+
+    override fun resize(width: Int, height: Int) {
+        gameViewport.update(width, height, true)
+    }
+
+    override fun render() {
+        clearScreen(0f, 0f, 0f, 1f)
+
+        val deltaTime = Gdx.graphics.deltaTime.coerceAtMost(1 / 30f)
+        world.update(deltaTime)
+    }
+
+    override fun dispose() {
+        batch.disposeSafely()
+        world.dispose()
+        assetService.disposeSafely()
+        tiledMap.disposeSafely()
+    }
+
+}
