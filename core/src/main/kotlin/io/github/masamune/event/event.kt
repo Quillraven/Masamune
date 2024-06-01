1fdd1b7cf4ca6921b1571381c163f6bd9f4f4f51
Quillraven <quillraven@gmail.com>
2024-06-02 00:06:04 +0200

    new interact events

--- core/src/main/kotlin/io/github/masamune/event/event.kt
+++ core/src/main/kotlin/io/github/masamune/event/event.kt
@@ -2,9 +2,16 @@ package io.github.masamune.event
 
 import com.badlogic.gdx.maps.tiled.TiledMap
 import com.badlogic.gdx.math.Vector2
+import com.github.quillraven.fleks.Entity
 
 sealed interface Event
 
 data class MapChangeEvent(val tiledMap: TiledMap) : Event
 
 data class PlayerMoveEvent(val direction: Vector2) : Event
+
+data class PlayerInteractBeginContactEvent(val player: Entity, val other: Entity) : Event
+
+data class PlayerInteractEndContactEvent(val player: Entity, val other: Entity) : Event
+
+data object PlayerInteractEvent : Event
