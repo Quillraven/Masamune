31da9656ef7a0e6d7008f5dca94df5209d004b3c
Quillraven <quillraven@gmail.com>
2024-06-02 00:08:20 +0200

    new physic contact listener with interact entity handling

---
+++ core/src/main/kotlin/io/github/masamune/PhysicContactHandler.kt
@@ -0,0 +1,65 @@
+package io.github.masamune
+
+import com.badlogic.gdx.physics.box2d.*
+import com.github.quillraven.fleks.Entity
+import com.github.quillraven.fleks.World
+import io.github.masamune.component.Tag
+import io.github.masamune.event.EventService
+import io.github.masamune.event.PlayerInteractBeginContactEvent
+import io.github.masamune.event.PlayerInteractEndContactEvent
+
+private interface ContactAdapter : ContactListener {
+    override fun beginContact(contact: Contact) = Unit
+
+    override fun endContact(contact: Contact) = Unit
+
+    override fun preSolve(contact: Contact, oldManifold: Manifold) = Unit
+
+    override fun postSolve(contact: Contact, impulse: ContactImpulse) = Unit
+}
+
+class PhysicContactHandler(
+    private val eventService: EventService,
+    private val world: World,
+) : ContactAdapter {
+    override fun beginContact(contact: Contact) {
+        if (!contact.isPlayerInteract()) {
+            return
+        }
+
+        eventService.fire(PlayerInteractBeginContactEvent(contact.playerEntity, contact.interactEntity))
+    }
+
+    override fun endContact(contact: Contact) {
+        if (!contact.isPlayerInteract()) {
+            return
+        }
+
+        eventService.fire(PlayerInteractEndContactEvent(contact.playerEntity, contact.interactEntity))
+    }
+
+    private fun Contact.isPlayerInteract(): Boolean {
+        return (fixtureA.isInteract() && fixtureA.isPlayerEntity() && fixtureB.isNonPlayerEntity())
+            || (fixtureB.isInteract() && fixtureB.isPlayerEntity() && fixtureA.isNonPlayerEntity())
+    }
+
+    private fun Fixture.isInteract(): Boolean {
+        return "interact" == userData
+    }
+
+    private fun Fixture.isPlayerEntity(): Boolean = with(world) {
+        val userData = body.userData
+        return userData is Entity && userData has Tag.PLAYER
+    }
+
+    private fun Fixture.isNonPlayerEntity(): Boolean = with(world) {
+        val userData = body.userData
+        return userData is Entity && userData hasNo Tag.PLAYER
+    }
+
+    private val Contact.playerEntity: Entity
+        get() = if (fixtureA.isPlayerEntity()) fixtureA.body.userData as Entity else fixtureB.body.userData as Entity
+
+    private val Contact.interactEntity: Entity
+        get() = if (fixtureA.isNonPlayerEntity()) fixtureA.body.userData as Entity else fixtureB.body.userData as Entity
+}
