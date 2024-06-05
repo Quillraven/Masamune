df0e7bcb85660cd4c72de4390ac004faa6004726
Quillraven <quillraven@gmail.com>
2024-06-05 07:39:52 +0200

    add ShaderService to LazyServiceLocator and update related tests

--- core/src/main/kotlin/io/github/masamune/ServiceLocator.kt
+++ core/src/main/kotlin/io/github/masamune/ServiceLocator.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.Disposable
 import io.github.masamune.asset.AssetService
+import io.github.masamune.asset.ShaderService
 import io.github.masamune.event.EventService
 import io.github.masamune.tiledmap.TiledService
 import ktx.assets.disposeSafely
@@ -13,6 +14,7 @@ interface ServiceLocator : Disposable {
     val asset: AssetService
     val event: EventService
     val tiled: TiledService
+    val shader: ShaderService
 }
 
 class LazyServiceLocator(
@@ -22,16 +24,19 @@ class LazyServiceLocator(
     tiledServiceInitializer: (AssetService, EventService) -> TiledService = { assetService, eventService ->
         TiledService(assetService, eventService)
     },
+    shaderServiceInitializer: () -> ShaderService = { ShaderService() },
 ) : ServiceLocator {
 
     override val batch: Batch by lazy(batchInitializer)
     override val asset: AssetService by lazy(assetServiceInitializer)
     override val event: EventService by lazy(eventServiceInitializer)
     override val tiled: TiledService by lazy { tiledServiceInitializer(asset, event) }
+    override val shader: ShaderService by lazy { shaderServiceInitializer() }
 
     override fun dispose() {
         batch.disposeSafely()
         asset.disposeSafely()
+        shader.disposeSafely()
     }
 
 }
--- core/src/test/kotlin/io/github/masamune/LazyServiceLocatorTest.kt
+++ core/src/test/kotlin/io/github/masamune/LazyServiceLocatorTest.kt
@@ -2,6 +2,7 @@ package io.github.masamune
 
 import com.badlogic.gdx.graphics.g2d.Batch
 import io.github.masamune.asset.AssetService
+import io.github.masamune.asset.ShaderService
 import io.github.masamune.event.EventService
 import io.github.masamune.tiledmap.TiledService
 import io.kotest.matchers.shouldNotBe
@@ -13,27 +14,53 @@ import kotlin.test.Test
 class LazyServiceLocatorTest {
 
     @Test
-    fun `verify TiledService gets correct asset and event service instance`() {
+    fun `verify that initializer args are properly called`() {
         val batchInitializer = mockk<() -> Batch>()
         val assetServiceInitializer = mockk<() -> AssetService>()
         val eventServiceInitializer = mockk<() -> EventService>()
+        val tiledServiceInitializer = mockk<(AssetService, EventService) -> TiledService>()
+        val shaderServiceInitializer = mockk<() -> ShaderService>()
         val batch = mockk<Batch>()
         val assetService = mockk<AssetService>()
         val eventService = mockk<EventService>()
+        val tiledService = mockk<TiledService>()
+        val shaderService = mockk<ShaderService>()
         every { batchInitializer.invoke() } returns batch
         every { assetServiceInitializer.invoke() } returns assetService
         every { eventServiceInitializer.invoke() } returns eventService
+        every { tiledServiceInitializer.invoke(assetService, eventService) } returns tiledService
+        every { shaderServiceInitializer.invoke() } returns shaderService
 
         val serviceLocator = LazyServiceLocator(
             batchInitializer,
             assetServiceInitializer,
             eventServiceInitializer,
-            tiledServiceInitializer = { asset, event -> TiledService(asset, event) }
+            tiledServiceInitializer,
+            shaderServiceInitializer
         )
 
         batch shouldBeSameInstanceAs serviceLocator.batch
         assetService shouldBeSameInstanceAs serviceLocator.asset
         eventService shouldBeSameInstanceAs serviceLocator.event
+        tiledService shouldBeSameInstanceAs serviceLocator.tiled
+        shaderService shouldBeSameInstanceAs serviceLocator.shader
+    }
+
+    @Test
+    fun `verify TiledService gets correct asset and event service instance`() {
+        val assetServiceInitializer = mockk<() -> AssetService>()
+        val eventServiceInitializer = mockk<() -> EventService>()
+        val assetService = mockk<AssetService>()
+        val eventService = mockk<EventService>()
+        every { assetServiceInitializer.invoke() } returns assetService
+        every { eventServiceInitializer.invoke() } returns eventService
+
+        val serviceLocator = LazyServiceLocator(
+            assetServiceInitializer = assetServiceInitializer,
+            eventServiceInitializer = eventServiceInitializer,
+            tiledServiceInitializer = { asset, event -> TiledService(asset, event) }
+        )
+
         serviceLocator.tiled shouldNotBe null
         serviceLocator.tiled.assetService shouldBeSameInstanceAs serviceLocator.asset
         serviceLocator.tiled.eventService shouldBeSameInstanceAs serviceLocator.event
