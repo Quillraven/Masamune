eba509276616df976035260b17cdd426fedf1186
Quillraven <quillraven@gmail.com>
2024-06-09 00:50:21 +0200

    add Dialog core classes and DSL

---
+++ core/src/main/kotlin/io/github/masamune/dialog/dialog.kt
@@ -0,0 +1,53 @@
+package io.github.masamune.dialog
+
+import ktx.app.gdxError
+
+sealed interface Action
+
+data object ActionNext : Action
+
+data object ActionExit : Action
+
+data object ActionPrevious : Action
+
+data class ActionGoTo(val pageIdx: Int) : Action
+
+data class Option(
+    val text: String,
+    val action: Action,
+)
+
+data class Page(
+    val pageIdx: Int,
+    val text: String,
+    val options: List<Option>,
+    val image: String? = null,
+)
+
+data class Dialog(val name: String, val pages: List<Page>) {
+    var activePage = pages.first()
+        private set
+
+    fun triggerOption(optionIdx: Int): Boolean {
+        if (optionIdx !in activePage.options.indices) {
+            gdxError("Dialog $name with page ${activePage.pageIdx} has no option with index $optionIdx")
+        }
+
+        when (val triggerAction = activePage.options[optionIdx].action) {
+            is ActionExit -> return true
+            is ActionNext -> goToPage(activePage.pageIdx + 1)
+            is ActionPrevious -> goToPage(activePage.pageIdx - 1)
+            is ActionGoTo -> goToPage(triggerAction.pageIdx)
+        }
+
+        return false
+    }
+
+    private fun goToPage(pageIdx: Int) {
+        if (pageIdx !in pages.indices) {
+            gdxError("Dialog $name with page ${activePage.pageIdx} links to invalid page $pageIdx")
+        }
+
+        activePage = pages[pageIdx]
+    }
+}
---
+++ core/src/test/kotlin/io/github/masamune/dialog/DialogTest.kt
@@ -0,0 +1,170 @@
+package io.github.masamune.dialog
+
+import com.badlogic.gdx.utils.GdxRuntimeException
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeSameInstanceAs
+import kotlin.test.Test
+
+class DialogTest {
+
+    @Test
+    fun `empty dialog should throw a DialogCfgException`() {
+        shouldThrow<DialogCfgException> {
+            dialog("testDialog") {}
+        }
+    }
+
+    @Test
+    fun `page without options should throw a PageCfgException`() {
+        shouldThrow<PageCfgException> {
+            dialog("testDialog") {
+                page("First page") {}
+            }
+        }
+    }
+
+    @Test
+    fun `dialog without exit action should throw a DialogCfgException`() {
+        shouldThrow<DialogCfgException> {
+            dialog("testDialog") {
+                page("First page") {
+                    option("Next", ActionNext)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `should create dialog with one page and an exit action`() {
+        val dialog = dialog("testDialog") {
+            page("First") {
+                option("Quit", ActionExit)
+            }
+        }
+
+        dialog.name shouldBe "testDialog"
+        dialog.pages.size shouldBe 1
+        val firstPage = dialog.pages.first()
+        firstPage.pageIdx shouldBe 0
+        firstPage.text shouldBe "First"
+        firstPage.image shouldBe null
+        firstPage.options.size shouldBe 1
+        val firstOption = firstPage.options.first()
+        firstOption.text shouldBe "Quit"
+        firstOption.action shouldBeSameInstanceAs ActionExit
+    }
+
+    @Test
+    fun `trigger option of invalid index should throw an error`() {
+        val dialog = dialog("testDialog") {
+            page("First") {
+                option("Quit", ActionExit)
+            }
+        }
+
+        shouldThrow<GdxRuntimeException> {
+            dialog.triggerOption(-1)
+        }
+    }
+
+    @Test
+    fun `trigger 'next' option of last page should throw an error`() {
+        val dialog = dialog("testDialog") {
+            page("First") {
+                option("Next", ActionNext)
+                option("Quit", ActionExit)
+            }
+        }
+
+        shouldThrow<GdxRuntimeException> {
+            dialog.triggerOption(0)
+        }
+    }
+
+    @Test
+    fun `trigger 'previous' option of first page should throw an error`() {
+        val dialog = dialog("testDialog") {
+            page("First") {
+                option("Prev", ActionPrevious)
+                option("Quit", ActionExit)
+            }
+        }
+
+        shouldThrow<GdxRuntimeException> {
+            dialog.triggerOption(0)
+        }
+    }
+
+    @Test
+    fun `trigger 'next' option of page 1 changes dialog to page 2`() {
+        val dialog = dialog("testDialog") {
+            page("First") {
+                option("Next", ActionNext)
+            }
+            page("Second") {
+                option("Quit", ActionExit)
+            }
+        }
+
+        dialog.activePage.pageIdx shouldBe 0
+        val actual = dialog.triggerOption(0)
+
+        dialog.activePage.pageIdx shouldBe 1
+        actual shouldBe false
+    }
+
+    @Test
+    fun `trigger 'previous' option of page 2 changes dialog to page 1`() {
+        val dialog = dialog("testDialog") {
+            page("First") {
+                option("Next", ActionNext)
+            }
+            page("Second") {
+                option("Prev", ActionPrevious)
+                option("Quit", ActionExit)
+            }
+        }
+
+        dialog.triggerOption(0)
+        dialog.activePage.pageIdx shouldBe 1
+        val actual = dialog.triggerOption(0)
+
+        dialog.activePage.pageIdx shouldBe 0
+        actual shouldBe false
+    }
+
+    @Test
+    fun `trigger 'goTo 2' option of page 1 changes dialog to page 2`() {
+        val dialog = dialog("testDialog") {
+            page("First") {
+                option("GoTo", ActionGoTo(currentPageIdx + 1))
+            }
+
+            page("Second") {
+                option("Prev", ActionPrevious)
+                option("Quit", ActionExit)
+            }
+        }
+
+        val actual = dialog.triggerOption(0)
+
+        dialog.activePage.pageIdx shouldBe 1
+        actual shouldBe false
+    }
+
+    @Test
+    fun `trigger 'exit' option of a page returns true`() {
+        val dialog = dialog("testDialog") {
+            page("First") {
+                option("Quit", ActionExit)
+            }
+        }
+
+        val actual = dialog.triggerOption(0)
+
+        dialog.activePage.pageIdx shouldBe 0
+        actual shouldBe true
+    }
+
+}
