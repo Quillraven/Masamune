d7fb5384b40090a259745154fa3b29e93428f6a4
Quillraven <quillraven@gmail.com>
2024-06-20 00:01:22 +0200

    add ControllerState to handle different stages of the game in a better way (currently game and UI stage)

--- core/src/main/kotlin/io/github/masamune/input/KeyboardController.kt
+++ core/src/main/kotlin/io/github/masamune/input/KeyboardController.kt
@@ -1,30 +1,24 @@
 package io.github.masamune.input
 
 import com.badlogic.gdx.Input.Keys
-import io.github.masamune.event.EventService
-import io.github.masamune.event.PlayerInteractEvent
-import io.github.masamune.event.PlayerMoveEvent
-import io.github.masamune.input.KeyboardController.Command.*
+import io.github.masamune.event.*
 import ktx.app.KtxInputAdapter
 import ktx.math.vec2
 
-class KeyboardController(private val eventService: EventService) : KtxInputAdapter {
+private enum class Command {
+    LEFT,
+    RIGHT,
+    DOWN,
+    UP,
+    SELECT
+}
 
-    enum class Command {
-        MOVE_LEFT,
-        MOVE_RIGHT,
-        MOVE_DOWN,
-        MOVE_UP,
-        INTERACT
-    }
+private sealed interface ControllerState {
+    fun keyDown(command: Command)
+    fun keyUp(command: Command)
+}
 
-    private val keyMapping = mapOf(
-        Keys.A to MOVE_LEFT,
-        Keys.D to MOVE_RIGHT,
-        Keys.W to MOVE_UP,
-        Keys.S to MOVE_DOWN,
-        Keys.SPACE to INTERACT,
-    )
+private class ControllerStateGame(private val eventService: EventService) : ControllerState {
 
     private val direction = vec2()
 
@@ -34,34 +28,70 @@ class KeyboardController(private val eventService: EventService) : KtxInputAdapt
         eventService.fire(PlayerMoveEvent(direction))
     }
 
+    override fun keyDown(command: Command) = when (command) {
+        Command.LEFT -> updateMove(x = -1f)
+        Command.RIGHT -> updateMove(x = 1f)
+        Command.DOWN -> updateMove(y = -1f)
+        Command.UP -> updateMove(y = 1f)
+        Command.SELECT -> eventService.fire(PlayerInteractEvent)
+    }
+
+    override fun keyUp(command: Command) = when (command) {
+        Command.LEFT -> updateMove(x = 1f)
+        Command.RIGHT -> updateMove(x = -1f)
+        Command.DOWN -> updateMove(y = 1f)
+        Command.UP -> updateMove(y = -1f)
+        else -> Unit
+    }
+}
+
+private class ControllerStateUI(private val eventService: EventService) : ControllerState {
+    override fun keyDown(command: Command) = when (command) {
+        Command.UP -> eventService.fire(UiUpEvent)
+        Command.DOWN -> eventService.fire(UiDownEvent)
+        Command.SELECT -> eventService.fire(UiSelectEvent)
+        else -> Unit
+    }
+
+    override fun keyUp(command: Command) = Unit
+}
+
+class KeyboardController(eventService: EventService) : KtxInputAdapter, EventListener {
+
+    private val gameState = ControllerStateGame(eventService)
+    private val uiState = ControllerStateUI(eventService)
+    private var activeState: ControllerState = gameState
+
+    private val keyMapping = mapOf(
+        Keys.A to Command.LEFT,
+        Keys.D to Command.RIGHT,
+        Keys.W to Command.UP,
+        Keys.S to Command.DOWN,
+        Keys.SPACE to Command.SELECT,
+    )
+
     override fun keyDown(keycode: Int): Boolean {
         keyMapping[keycode]?.let { cmd ->
-            when (cmd) {
-                MOVE_LEFT -> updateMove(x = -1f)
-                MOVE_RIGHT -> updateMove(x = 1f)
-                MOVE_DOWN -> updateMove(y = -1f)
-                MOVE_UP -> updateMove(y = 1f)
-                INTERACT -> eventService.fire(PlayerInteractEvent)
-            }
+            activeState.keyDown(cmd)
             return true
         }
-
         return false
     }
 
     override fun keyUp(keycode: Int): Boolean {
         keyMapping[keycode]?.let { cmd ->
-            when (cmd) {
-                MOVE_LEFT -> updateMove(x = 1f)
-                MOVE_RIGHT -> updateMove(x = -1f)
-                MOVE_DOWN -> updateMove(y = 1f)
-                MOVE_UP -> updateMove(y = -1f)
-                else -> return true
-            }
+            activeState.keyUp(cmd)
             return true
         }
-
         return false
+    }
+
+    override fun onEvent(event: Event) {
+        when (event) {
+            is DialogBeginEvent -> activeState = uiState
+            is DialogEndEvent -> activeState = gameState
+            else -> Unit
+        }
     }
 
 }
