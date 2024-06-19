451be4ae95c331f06c2c922b677f4f91ed68fddf
Quillraven <quillraven@gmail.com>
2024-06-20 00:02:58 +0200

    add dialog cache and method to create dialogs by name

---
+++ core/src/main/kotlin/io/github/masamune/dialog/dialogCfg.kt
@@ -0,0 +1,36 @@
+package io.github.masamune.dialog
+
+import ktx.app.gdxError
+import ktx.log.Logger
+
+private val dialogCache = mutableListOf<Dialog>()
+private val log = Logger("DialogCfg")
+
+fun dialogOf(name: String): Dialog {
+    val cachedDialog = dialogCache.firstOrNull { it.name == name }
+    if (cachedDialog != null) {
+        return cachedDialog
+    }
+
+    log.debug { "Creating new dialog $name" }
+    if (dialogCache.size >= 4) {
+        log.debug { "Removing one dialog of the cache" }
+        dialogCache.removeFirst()
+    }
+
+    val newDialog: Dialog = dialogByName(name)
+
+    dialogCache += newDialog
+    return newDialog
+}
+
+private fun dialogByName(name: String) = when (name) {
+    "elder_00" -> dialog(name) {
+        page("{SLOW}{FADE}Hello hero", "elder", "Elder") {
+            option("OK", ActionExit)
+            option("Quit", ActionExit)
+        }
+    }
+
+    else -> gdxError("There is no dialog configured for name $name")
+}
