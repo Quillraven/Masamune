0bfb175594271c4fbad1bdfb8ede8beaedde28dd
Quillraven <quillraven@gmail.com>
2024-06-20 07:17:06 +0200

    optimize input handling to not trigger UP events if the DOWN event of a key was not happening before for the active controller state

---
+++ core/src/main/kotlin/io/github/masamune/input/ControllerState.kt
@@ -0,0 +1,52 @@
+package io.github.masamune.input
+
+import io.github.masamune.event.*
+import ktx.math.vec2
+
+sealed interface ControllerState {
+    fun keyDown(command: Command)
+    fun keyUp(command: Command)
+    fun onActive() = Unit
+}
+
+class ControllerStateGame(private val eventService: EventService) : ControllerState {
+
+    private val direction = vec2()
+
+    override fun onActive() {
+        direction.setZero()
+    }
+
+    private fun updateMove(x: Float = 0f, y: Float = 0f) {
+        direction.x += x
+        direction.y += y
+        eventService.fire(PlayerMoveEvent(direction))
+    }
+
+    override fun keyDown(command: Command) = when (command) {
+        Command.LEFT -> updateMove(x = -1f)
+        Command.RIGHT -> updateMove(x = 1f)
+        Command.DOWN -> updateMove(y = -1f)
+        Command.UP -> updateMove(y = 1f)
+        Command.SELECT -> eventService.fire(PlayerInteractEvent)
+    }
+
+    override fun keyUp(command: Command) = when (command) {
+        Command.LEFT -> updateMove(x = 1f)
+        Command.RIGHT -> updateMove(x = -1f)
+        Command.DOWN -> updateMove(y = 1f)
+        Command.UP -> updateMove(y = -1f)
+        else -> Unit
+    }
+}
+
+class ControllerStateUI(private val eventService: EventService) : ControllerState {
+    override fun keyDown(command: Command) = when (command) {
+        Command.UP -> eventService.fire(UiUpEvent)
+        Command.DOWN -> eventService.fire(UiDownEvent)
+        Command.SELECT -> eventService.fire(UiSelectEvent)
+        else -> Unit
+    }
+
+    override fun keyUp(command: Command) = Unit
+}
--- core/src/main/kotlin/io/github/masamune/input/KeyboardController.kt
+++ core/src/main/kotlin/io/github/masamune/input/KeyboardController.kt
@@ -3,64 +3,20 @@ package io.github.masamune.input
 import com.badlogic.gdx.Input.Keys
 import io.github.masamune.event.*
 import ktx.app.KtxInputAdapter
-import ktx.math.vec2
-
-private enum class Command {
-    LEFT,
-    RIGHT,
-    DOWN,
-    UP,
-    SELECT
-}
-
-private sealed interface ControllerState {
-    fun keyDown(command: Command)
-    fun keyUp(command: Command)
-}
-
-private class ControllerStateGame(private val eventService: EventService) : ControllerState {
-
-    private val direction = vec2()
-
-    private fun updateMove(x: Float = 0f, y: Float = 0f) {
-        direction.x += x
-        direction.y += y
-        eventService.fire(PlayerMoveEvent(direction))
-    }
-
-    override fun keyDown(command: Command) = when (command) {
-        Command.LEFT -> updateMove(x = -1f)
-        Command.RIGHT -> updateMove(x = 1f)
-        Command.DOWN -> updateMove(y = -1f)
-        Command.UP -> updateMove(y = 1f)
-        Command.SELECT -> eventService.fire(PlayerInteractEvent)
-    }
-
-    override fun keyUp(command: Command) = when (command) {
-        Command.LEFT -> updateMove(x = 1f)
-        Command.RIGHT -> updateMove(x = -1f)
-        Command.DOWN -> updateMove(y = 1f)
-        Command.UP -> updateMove(y = -1f)
-        else -> Unit
-    }
-}
-
-private class ControllerStateUI(private val eventService: EventService) : ControllerState {
-    override fun keyDown(command: Command) = when (command) {
-        Command.UP -> eventService.fire(UiUpEvent)
-        Command.DOWN -> eventService.fire(UiDownEvent)
-        Command.SELECT -> eventService.fire(UiSelectEvent)
-        else -> Unit
-    }
-
-    override fun keyUp(command: Command) = Unit
-}
 
 class KeyboardController(eventService: EventService) : KtxInputAdapter, EventListener {
 
+    private val commandState = booleanArrayOf(*Command.entries.map { false }.toBooleanArray())
     private val gameState = ControllerStateGame(eventService)
     private val uiState = ControllerStateUI(eventService)
     private var activeState: ControllerState = gameState
+        set(value) {
+            for (i in commandState.indices) {
+                commandState[i] = false
+            }
+            value.onActive()
+            field = value
+        }
 
     private val keyMapping = mapOf(
         Keys.A to Command.LEFT,
@@ -72,6 +28,7 @@ class KeyboardController(eventService: EventService) : KtxInputAdapter, EventLis
 
     override fun keyDown(keycode: Int): Boolean {
         keyMapping[keycode]?.let { cmd ->
+            commandState[cmd] = true
             activeState.keyDown(cmd)
             return true
         }
@@ -80,6 +37,12 @@ class KeyboardController(eventService: EventService) : KtxInputAdapter, EventLis
 
     override fun keyUp(keycode: Int): Boolean {
         keyMapping[keycode]?.let { cmd ->
+            if (!commandState[cmd]) {
+                // button was not pressed before in the current state -> ignore it
+                return false
+            }
+
+            commandState[cmd.ordinal] = false
             activeState.keyUp(cmd)
             return true
         }
@@ -92,6 +55,12 @@ class KeyboardController(eventService: EventService) : KtxInputAdapter, EventLis
             is DialogEndEvent -> activeState = gameState
             else -> Unit
         }
+    }
+
+    private operator fun BooleanArray.get(command: Command) = this[command.ordinal]
+
+    private operator fun BooleanArray.set(command: Command, value: Boolean) {
+        this[command.ordinal] = value
     }
 
 }
