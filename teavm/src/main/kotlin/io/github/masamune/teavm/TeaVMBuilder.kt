71af530c6680a593d2b6a4da5ffdc3a01f421cdb
Quillraven <quillraven@gmail.com>
2024-06-23 00:22:53 +0200

    add scene2d style and typing label classes that require reflection

--- teavm/src/main/kotlin/io/github/masamune/teavm/TeaVMBuilder.kt
+++ teavm/src/main/kotlin/io/github/masamune/teavm/TeaVMBuilder.kt
@@ -2,7 +2,12 @@ package io.github.masamune.teavm
 
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuildConfiguration
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuilder
+import com.github.xpenatan.gdx.backends.teavm.config.plugins.TeaReflectionSupplier
 import com.github.xpenatan.gdx.backends.teavm.gen.SkipClass
+import com.rafaskoberg.gdx.typinglabel.TypingGlyph
+import com.rafaskoberg.gdx.typinglabel.effects.FadeEffect
+import io.github.masamune.ui.view.DialogViewStyle
+import io.github.masamune.ui.widget.DialogOptionStyle
 import java.io.File
 
 /** Builds the TeaVM/HTML application. */
@@ -21,7 +26,12 @@ object TeaVMBuilder {
         }
 
         // Register any classes or packages that require reflection here:
-        // TeaReflectionSupplier.addReflectionClass("io.github.masamune.reflect")
+        // Scene2D style classes require reflection
+        TeaReflectionSupplier.addReflectionClass(DialogViewStyle::class.java)
+        TeaReflectionSupplier.addReflectionClass(DialogOptionStyle::class.java)
+        // Typing label effects require reflection
+        TeaReflectionSupplier.addReflectionClass(TypingGlyph::class.java)
+        TeaReflectionSupplier.addReflectionClass(FadeEffect::class.java)
 
         val tool = TeaBuilder.config(teaBuildConfiguration)
         tool.mainClass = "io.github.masamune.teavm.TeaVMLauncher"
