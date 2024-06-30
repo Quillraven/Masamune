27a1f1e457d239aea92d6aa04240551fdc70733a
Quillraven <quillraven@gmail.com>
2024-06-30 19:40:14 +0200

    add documentation to asset classes

--- core/src/main/kotlin/io/github/masamune/asset/ShaderService.kt
+++ core/src/main/kotlin/io/github/masamune/asset/ShaderService.kt
@@ -12,18 +12,31 @@ import ktx.assets.disposeSafely
 import ktx.graphics.use
 import ktx.math.vec2
 
+/**
+ * Service class for [ShaderProgram] management. Supports loading and usage of following shaders:
+ * - outline
+ *
+ * If the loading of a shader fails then an exception with the shader's error log is thrown.
+ */
 class ShaderService(private val fileHandleResolver: FileHandleResolver = InternalFileHandleResolver()) : Disposable {
 
     private lateinit var outlineShader: ShaderProgram
     private var outlineColorIdx = -1
     private var outlinePixelSizeIdx = -1
 
+    /**
+     * Loads all shaders and stores uniform locations internally for better performance.
+     */
     fun loadAllShader() {
         outlineShader = loadShader("default.vert", "outline.frag")
         outlineColorIdx = outlineShader.getUniformLocation("u_outlineColor")
         outlinePixelSizeIdx = outlineShader.getUniformLocation("u_pixelSize")
     }
 
+    /**
+     * Loads a specific shader via its [vertexFile] and [fragmentFile].
+     * If loading fails then an exception with the shader's error log is thrown.
+     */
     private fun loadShader(vertexFile: String, fragmentFile: String): ShaderProgram {
         val vertexCode = fileHandleResolver.resolve("shader/$vertexFile").readString()
         val fragmentCode = fileHandleResolver.resolve("shader/$fragmentFile").readString()
@@ -34,6 +47,13 @@ class ShaderService(private val fileHandleResolver: FileHandleResolver = Interna
         return shader
     }
 
+    /**
+     * Sets an **outline** shader as active shader for any draw calls of the [batch].
+     * The line has the given [color].
+     * The [texture] for the draw calls must be provided to correctly calculate the size of a pixel
+     * inside the shader program.
+     * After the draw [block] is executed, the previous shader of the [batch] is restored.
+     */
     fun useOutlineShader(batch: Batch, color: Color, texture: Texture, block: () -> Unit) {
         // set outline shader and its uniforms
         val origShader = batch.shader
@@ -52,6 +72,9 @@ class ShaderService(private val fileHandleResolver: FileHandleResolver = Interna
         batch.shader = origShader
     }
 
+    /**
+     * Disposes all shaders.
+     */
     override fun dispose() {
         outlineShader.disposeSafely()
     }
--- core/src/main/kotlin/io/github/masamune/asset/assets.kt
+++ core/src/main/kotlin/io/github/masamune/asset/assets.kt
@@ -17,6 +17,9 @@ import ktx.assets.load
 import ktx.log.logger
 import java.util.*
 
+/**
+ * Asset type for [TextureAtlas] instances. Used by [AssetService].
+ */
 enum class AtlasAsset(folder: String = "graphics") {
     CHARS_AND_PROPS,
     SFX,
@@ -25,12 +28,18 @@ enum class AtlasAsset(folder: String = "graphics") {
     val path = "$folder/${name.lowercase()}.atlas"
 }
 
+/**
+ * Asset type for [TiledMap] instances. Used by [AssetService].
+ */
 enum class TiledMapAsset {
     VILLAGE;
 
     val path = "maps/${name.lowercase()}.tmx"
 }
 
+/**
+ * Asset type for [Skin] instances. Used by [AssetService].
+ */
 enum class SkinAsset(skinName: String, atlas: AtlasAsset) {
     DEFAULT("skin", AtlasAsset.SKIN);
 
@@ -38,50 +47,98 @@ enum class SkinAsset(skinName: String, atlas: AtlasAsset) {
     val atlasPath = atlas.path
 }
 
+/**
+ * Asset type for [I18NBundle] instances. Used by [AssetService].
+ */
 enum class I18NAsset {
     MESSAGES;
 
     val path = "ui/${name.lowercase()}"
 }
 
+/**
+ * Service class for asset management. It supports loading and unloading of:
+ * - [AtlasAsset]
+ * - [TiledMapAsset]
+ * - [SkinAsset]
+ * - [I18NAsset]
+ *
+ * Loading is done asynchronously. Call [finishLoading] to immediately load any queued assets.
+ */
 class AssetService(fileHandleResolver: FileHandleResolver = InternalFileHandleResolver()) : Disposable {
 
     private val manager = AssetManager(fileHandleResolver).apply {
         setLoader(TiledMap::class.java, TmxMapLoader(this.fileHandleResolver))
     }
 
+    /**
+     * Queues an [AtlasAsset] for loading.
+     */
     fun load(asset: AtlasAsset) {
         manager.load<TextureAtlas>(asset.path)
     }
 
+    /**
+     * Gets a loaded [AtlasAsset] as [TextureAtlas].
+     */
     operator fun get(asset: AtlasAsset): TextureAtlas = manager.getAsset<TextureAtlas>(asset.path)
 
+    /**
+     * Queues a [TiledMapAsset] for loading.
+     */
     fun load(asset: TiledMapAsset) {
         manager.load<TiledMap>(asset.path)
     }
 
+    /**
+     * Queues a [TiledMapAsset] for unloading.
+     */
     fun unload(asset: TiledMapAsset) {
         manager.unload(asset.path)
     }
 
+    /**
+     * Gets a loaded [TiledMapAsset] as [TiledMap].
+     */
     operator fun get(asset: TiledMapAsset): TiledMap = manager.getAsset<TiledMap>(asset.path)
 
+    /**
+     * Queues a [SkinAsset] for loading.
+     */
     fun load(asset: SkinAsset) {
         manager.load<Skin>(asset.path, SkinParameter(asset.atlasPath))
     }
 
+    /**
+     * Gets a loaded [SkinAsset] as [Skin].
+     */
     operator fun get(asset: SkinAsset): Skin = manager.getAsset<Skin>(asset.path)
 
+    /**
+     * Queues a [I18NAsset] for loading.
+     */
     fun load(asset: I18NAsset) {
         manager.load<I18NBundle>(asset.path, I18NBundleParameter(Locale.getDefault(), Charsets.ISO_8859_1.name()))
     }
 
+    /**
+     * Gets a loaded [I18NAsset] as [I18NBundle].
+     */
     operator fun get(asset: I18NAsset): I18NBundle = manager.getAsset<I18NBundle>(asset.path)
 
+    /**
+     * Updates the loading process of any queued asset.
+     */
     fun update(): Boolean = manager.update(1 / 60 * 1000)
 
+    /**
+     * Immediately finishes loading of any queued asset.
+     */
     fun finishLoading() = manager.finishLoading()
 
+    /**
+     * Disposes all assets and stop all loading processes.
+     */
     override fun dispose() {
         log.debug { "Disposing AssetService:\n${manager.diagnostics}" }
         manager.disposeSafely()
