d7af186b13ba79a0112d2d8097c0151508795373
Quillraven <Quillraven@users.noreply.github.com>
2024-07-28 20:21:57 +0000

    Deploying to gh-pages from @ Quillraven/Masamune@2f632a8544def1fa2effec2a432af9179d06735a 🚀

--- core/src/main/kotlin/io/github/masamune/component/Tag.kt
+++
@@ -1,8 +0,0 @@
-package io.github.masamune.component
-
-import com.github.quillraven.fleks.EntityTags
-import com.github.quillraven.fleks.entityTagOf
-
-enum class Tag : EntityTags by entityTagOf() {
-    CAMERA_FOCUS, EXECUTE_TRIGGER, ENEMY
-}
