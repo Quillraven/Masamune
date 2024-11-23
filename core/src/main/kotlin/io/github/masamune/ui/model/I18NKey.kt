4ca3036cf04613304ec002b616f6637378a30212
Quillraven <quillraven@gmail.com>
2024-11-23 01:12:23 +0100

    create enum for message keys

---
+++ core/src/main/kotlin/io/github/masamune/ui/model/I18NKey.kt
@@ -0,0 +1,8 @@
+package io.github.masamune.ui.model
+
+enum class I18NKey(val key: String) {
+    GENERAL_TOTAL("general.total"),
+    GENERAL_YES("general.yes"),
+    GENERAL_NO("general.no"),
+    SHOP_CONFIRM_BUY("shop.confirmBuy"),
+}
