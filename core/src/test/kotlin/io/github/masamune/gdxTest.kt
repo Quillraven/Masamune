000092e610290fe893947a3c3998d085c48314ee
Quillraven <quillraven@gmail.com>
2024-12-08 01:09:05 +0100

    add window size configuration

--- core/src/test/kotlin/io/github/masamune/gdxTest.kt
+++ core/src/test/kotlin/io/github/masamune/gdxTest.kt
@@ -3,10 +3,12 @@ package io.github.masamune
 import com.badlogic.gdx.ApplicationListener
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.badlogic.gdx.math.Vector2
+import ktx.math.vec2
 
-fun gdxTest(title: String, testListener: ApplicationListener) {
+fun gdxTest(title: String, testListener: ApplicationListener, windowSize: Vector2 = vec2(1280f, 960f)) {
     Lwjgl3Application(testListener, Lwjgl3ApplicationConfiguration().apply {
         setTitle(title)
-        setWindowedMode(1280, 960)
+        setWindowedMode(windowSize.x.toInt(), windowSize.y.toInt())
     })
 }
