4150711e11e89e505711c37326d3a8c1e47c3c45
Quillraven <quillraven@gmail.com>
2025-03-15 00:29:49 +0100

    reduce str booster factor and minor buff cyclops

--- core/src/main/kotlin/io/github/masamune/combat/action/StrBoosterAction.kt
+++ core/src/main/kotlin/io/github/masamune/combat/action/StrBoosterAction.kt
@@ -6,6 +6,6 @@ import io.github.masamune.tiledmap.ActionType
 
 class StrBoosterAction : Action(ActionType.STR_BOOSTER, ActionTargetType.NONE, defensive = true) {
     override fun ActionExecutorService.onCombatStart() {
-        addBuff(StrBoosterBuff(source, 1f))
+        addBuff(StrBoosterBuff(source, 0.25f))
     }
 }
